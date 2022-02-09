7285ef68c3a3d02e7a5c12cdf9bc488bccef0d7b
Maksim Ganin <maksim_ganin@epam.com>
2022-02-09 10:02:35 +0400

    create services interfaces

---
+++ src/main/java/marketplace/service/UserService.java
@@ -0,0 +1,12 @@
+package marketplace.service;
+
+import marketplace.model.User;
+
+public interface UserService {
+
+    void save(User user);
+
+    User findByUsername(String username);
+
+    User findByUserLogin(String userLogin);
+}
