18188217ab240854f02ef446a97a1288a34e18b4
Maksim Ganin <maksim_ganin@epam.com>
2022-02-09 09:55:54 +0400

    create dao classes

---
+++ src/main/java/marketplace/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package marketplace.repository;
+
+import marketplace.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserLogin(String userLogin);
+
+}
