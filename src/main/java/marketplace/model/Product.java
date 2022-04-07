07dfee51b24e394dab67e36ad892d39111b67d4c
Maksim Ganin <maksim_ganin@epam.com>
2022-04-07 17:23:03 +0400

    add model Product

---
+++ src/main/java/marketplace/model/Product.java
@@ -0,0 +1,46 @@
+package marketplace.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "owner_id")
+    private Long ownerId;
+
+    @Column(name = "offer_id")
+    private Long offerId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "start_price")
+    private int startPrice;
+
+    @Column(name = "start_date")
+    private Date startDate;
+
+    @Column(name = "end_date")
+    private Date endDate;
+
+    @Column(name = "bidinc")
+    private int bidInc;
+
+}
